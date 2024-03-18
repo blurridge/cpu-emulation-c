d8be6fed72782cb757bceda5c81edfdae2ddf86d
Zach Riane Machacon <zachriane01@gmail.com>
2024-03-18 09:02:06 +0800

    Created control unit file

---
+++ CU.c
@@ -0,0 +1,20 @@
+/*============================================================================================================
+FILENAME        :CU.c
+DESCRIPTION     :Emulate the function of a control unit using C/ C++ program
+AUTHOR          :Zach Riane I. Machacon, Eldridge Aaron S. Miole, Dave Nelson F. Ogue, John Jason C. Zamora
+CREATED ON      :27 February 2024
+============================================================================================================*/
+
+#include <stdio.h>
+#include <unistd.h>
+
+int CU(void);
+
+int main() {
+    return 0;
+}
+
+int CU(void) {
+    unsigned char PC, IR, MAR, MBR, IO_AR, IO_BR;
+    return 1;
+}
